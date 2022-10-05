e446894dba482739c09dbd5fc90efb331ce987e2
João Vitor Sales <97055811+Joao-Vitor-Sales@users.noreply.github.com>
2022-10-05 12:28:31 -0300

    Update ex2_2.js

--- cap2/js/ex2_2.js
+++ cap2/js/ex2_2.js
@@ -1,17 +1,11 @@
 function exibirFilme() {
     var titulo = document.getElementById("inTitulo").value;
-    var duracao = document.getElementById("inDuracao").value;
+    var inDuracao = document.getElementById("inDuracao").value;
+    var transformationHoras = inDuracao / 60;
+    var transformationMinutos = inDuracao % 60;
+    var duracao = Math.round(transformationHoras) + " hora(s) " + transformationMinutos + " minuto(s)";
     document.getElementById("outResposta").textContent = titulo + " " + duracao;
-
 }
 
 var mostrar = document.getElementById("mostrar");
 mostrar.addEventListener("click", exibirFilme);
-
-
-
-
-// var inDuracao = document.getElementById("inDuracao"); FALTOU O .VALUE
-// var transformationHoras = inDuracao / 60;
-// var transformationMinutos = inDuracao % 60;
-// var duracao = transformationHoras + "hora(s)" + tranformationMinutos + "minuto(s)";
